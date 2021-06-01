da3b50bf8ed3481bb5683da7c4be034b89f90b05
jborjon <work@josephborjon.com>
2021-06-01 11:52:21 -0600

    Exclude unminified scripts from site output

--- scripts/lazy-load-img.js
+++
@@ -1,8 +0,0 @@
-// Thanks to Rahul Nanwani at https://css-tricks.com/the-complete-guide-to-lazy-loading-images/
-(function () {
-  "use strict";
-
-  document.addEventListener("DOMContentLoaded", function () {
-    //
-  });
-}());
