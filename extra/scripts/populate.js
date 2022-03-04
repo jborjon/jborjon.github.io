999a2e2a8d8a350d8c4bf524d05a832edd965029
Joseph Borjon <work@josephborjon.com>
2022-03-04 15:23:11 -0700

    Update populate.js

--- extra/scripts/populate.js
+++ extra/scripts/populate.js
@@ -8,16 +8,24 @@
     }
 
     function createCards(phraseData) {
-        appendListItems(cardList, 2, "", phraseData.people);
-        appendListItems(cardList, 2, "The", phraseData.occupations);
-        appendListItems(cardList, 6, "", phraseData.things);
+        appendListItems(cardList, 2, false, phraseData.people);
+        appendListItems(cardList, 2, true, phraseData.occupations);
+        appendListItems(cardList, 6, false, phraseData.things);
     }
 
-    function appendListItems(cardList, numItems, prefix, arrayFrom) {
+    function appendListItems(cardList, numItems, hasArticle, sourceArray) {
         let cardListItem;
         for (let i = 0; i < numItems; ++i) {
             cardListItem = document.createElement("li");
-            cardListItem.textContent = `${prefix} ${pickRandomItem(arrayFrom)}`;
+
+            // Prepend 'the' or 'a' only if we know we need it
+            if (hasArticle) {
+                const article = Math.random < 0.5 ? "the" : "a";
+                cardListItem.textContent = `${article} ${pickRandomItem(sourceArray)}`;
+            } else {
+                cardListItem.textContent = pickRandomItem(sourceArray);
+            }
+
             cardList.appendChild(cardListItem);
         }
     }
